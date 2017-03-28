794042a9c4dfd5efb8266e9bcc312bc57d31577c
Jørgen Sundt <jorgen@theeleven.co.uk>
2017-03-28 18:22:17 +0100

    Initial test of icon left slice

--- lib/fenrir/slices.rb
+++ lib/fenrir/slices.rb
@@ -10,6 +10,8 @@ module Fenrir::Slices
     case type
     when 'content'
       ui_slice_content(options, content)
+    when 'icon_left'
+      ui_slice_icon_left(options, content)
     else
       raise "Slice of type: #{type} does not exist"
     end
@@ -25,6 +27,21 @@ module Fenrir::Slices
 
     content_tag(:div, class: "o-slice clearfix #{options[:class]}") do
       content
+    end
+  end
+
+  def ui_slice_icon_left(options, content)
+    options = {
+      icon: 'rocket',
+      icon_variant: 'float-left',
+      class: "u-padding-y-25"
+    }.merge(options)
+
+    content_tag(:div, class: "o-slice clearfix #{options[:class]}") do
+      render("objects/icons/#{options[:icon]}", variant: options[:icon_variant]) +
+      content_tag(:div, style: 'padding-left: 5px; float: left;') do
+        content
+      end
     end
   end
 
